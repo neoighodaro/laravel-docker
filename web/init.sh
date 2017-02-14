f0c01a455cc50a0304a70011aadc3e2c851e01f2
Neo Ighodaro <neo@creativitykills.co>
2017-02-14 06:46:37 +0100

    Updated auto .env population.

--- web/init.sh
+++ web/init.sh
@@ -11,9 +11,14 @@ sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=${MYSQL_PASSWORD}/" .env
 sed -i "s/DB_DATABASE=.*/DB_DATABASE=${MYSQL_DATABASE}/" .env
 
 # Update the Redis values in the .env file
-sed -i "s/REDIS_HOST=.*/REDIS_HOST=${REDIS_HOST}/"             .env
-sed -i "s/REDIS_PORT=.*/REDIS_PORT=${REDIS_PORT}/"             .env
-sed -i "s/REDIS_PASSWORD=.*/REDIS_PASSWORD=${REDIS_PASSWORD}/" .env
+sed -i "s/REDIS_HOST=.*/REDIS_HOST=${REDIS_HOST}/" .env
+sed -i "s/REDIS_PORT=.*/REDIS_PORT=${REDIS_PORT}/" .env
+
+# Drivers
+sed -i "s/CACHE_DRIVER=.*/CACHE_DRIVER=${CACHE_DRIVER}/"             .env
+sed -i "s/QUEUE_DRIVER=.*/QUEUE_DRIVER=${QUEUE_DRIVER}/"             .env
+sed -i "s/SESSION_DRIVER=.*/SESSION_DRIVER=${SESSION_DRIVER}/"       .env
+sed -i "s/BROADCAST_DRIVER=.*/BROADCAST_DRIVER=${BROADCAST_DRIVER}/" .env
 
 # Run the start file that comes from creativitykills/nginx-php-server
 /start.sh
