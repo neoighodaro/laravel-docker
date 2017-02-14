11cdb0ee416e544b826241df5f94313d2e1a1313
Neo Ighodaro <neo@creativitykills.co>
2017-02-14 05:47:44 +0100

    Added redis service.

--- web/init.sh
+++ web/init.sh
@@ -10,5 +10,10 @@ sed -i "s/DB_USERNAME=.*/DB_USERNAME=${MYSQL_USER}/"     .env
 sed -i "s/DB_PASSWORD=.*/DB_PASSWORD=${MYSQL_PASSWORD}/" .env
 sed -i "s/DB_DATABASE=.*/DB_DATABASE=${MYSQL_DATABASE}/" .env
 
+# Update the Redis values in the .env file
+sed -i "s/REDIS_HOST=.*/REDIS_HOST=${REDIS_HOST}/"             .env
+sed -i "s/REDIS_PORT=.*/REDIS_PORT=${REDIS_PORT}/"             .env
+sed -i "s/REDIS_PASSWORD=.*/REDIS_PASSWORD=${REDIS_PASSWORD}/" .env
+
 # Run the start file that comes from creativitykills/nginx-php-server
 /start.sh
